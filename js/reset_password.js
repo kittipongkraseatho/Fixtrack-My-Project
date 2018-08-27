5d424822f1903ccd12d89acfbdb1200294b3457f
Matino <kittipongbig@gmail.com>
2018-08-27 12:54:24 +0700

    master
    reset_pass

--- js/reset_password.js
+++ js/reset_password.js
@@ -1,18 +1,25 @@
 function c_password() {
     var email = document.getElementById('email').value;
+    // [START sendpasswordemail]
     firebase.auth().sendPasswordResetEmail(email).then(function() {
       // Password Reset Email Sent!
+      // [START_EXCLUDE]
       alert("กลับไปที่ Email ของท่าน");
+      // [END_EXCLUDE]
     }).catch(function(error) {
+      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
+      // [START_EXCLUDE]
       if (errorCode == 'auth/invalid-email') {
         alert(errorMessage);
       } else if (errorCode == 'auth/user-not-found') {
         alert(errorMessage);
       }
       console.log(error);
+      // [END_EXCLUDE]
     });
+    // [END sendpasswordemail];
   }
   
 
